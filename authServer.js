d2c1508f70f14fb0a2f0acbf14c3095f37cc16fd
samvdr_hi10 <stijnn.buys@gmail.com>
2021-04-27 20:11:12 +0200

    ssl

--- authServer.js
+++ authServer.js
@@ -4,6 +4,31 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/litwick/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/litwick/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/yourdomain.com/chain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +36,7 @@ const port = process.env.PORT || 4000;
 const indexRouter = require("./routes/index");
 const authenticationRouter = require("./routes/authentication");
 
+app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
@@ -18,11 +44,15 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.json());
 
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
 mongoose.connect(process.env.DATABASE_URL, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
-
-router.get("/users", async (req, res) => {
+router.get("/users", authenticateToken, async (req, res) => {
   try {
     const users = await User.find({});
     res.send({ users: users });
   } catch {}
 });
+
+function authenticateToken(req, res, nex) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    nex();
+  });
+}
 
 module.exports = router;
